3f086d3a99a7db8a82d141a2919d46899b834bf3
Punyapat Lertsamrong <punyapat1412@gmail.com>
2023-07-29 20:54:42 +0700

    add pages

--- client/alumni/src/App.js
+++ client/alumni/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import SignUp from './pages/SignUp/SignUp';
-
+import Navbar from './components/Navbar.tsx';
+import Login from './pages/Login/Login';
+import { BrowserRouter as Router, Routes, Route }from 'react-router-dom';
 function App() {
   return (
-  <SignUp/>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
+        <Route path='/login' element={<Login/>}/>
+      </Routes>
+      
+    </Router>
+    
   );
 }
 
--- client/alumni/src/pages/Login/Login.js
+++ client/alumni/src/pages/Login/Login.js
@@ -7,8 +7,8 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import { NavLink } from 'react-router-dom';
 import Box from '@mui/material/Box';
+import { NavLink } from 'react-router-dom';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
@@ -19,7 +19,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const defaultTheme = createTheme();
 
-export default function SignUp() {
+export default function Login() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -45,31 +45,10 @@ export default function SignUp() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Sign up
+            Sign in
           </Typography>
           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
               <Grid item xs={12}>
                 <TextField
                   required
@@ -96,14 +75,15 @@ export default function SignUp() {
               type="submit"
               fullWidth
               variant="contained"
+              style={{backgroundColor:"#a73b23"}}
               sx={{ mt: 3, mb: 2 }}
             >
               Sign Up
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <NavLink to="/login" variant="body2">
-                  Already have an account? Sign in
+                <NavLink to="/" variant="body2">
+                  Forget password?
                 </NavLink>
               </Grid>
             </Grid>
